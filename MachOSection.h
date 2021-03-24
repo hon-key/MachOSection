b06b9c90aabe425f459a467936f6f1c72ab4c6df
HJ-Cai <lj_chj@126.com>
2021-03-24 09:50:06 +0800

    Create MachOSection.h

---
+++ MachOSection.h
@@ -0,0 +1,36 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef struct MachOSectionItem {
+    void *value;
+    bool func;
+    NSString *tag;
+}MachOSectionItem;
+
+// Value Definition
+#define MACH_O_SECTION_VALUE_DEFINITION(KEY,VALUE,TAG) \
+__attribute__((used,section("__DATA" "," #KEY))) static const MachOSectionItem KEY##_v = {(void *)VALUE,false,false,TAG};
+
+// Function Definition
+#define MACH_O_SECTION_FUNCTION_DEFINITION_F(KEY,AUTORELEASE,TAG) \
+static void KEY(void); \ 
+__attribute__((used,section("__DATA" "," #KEY))) static const MachOSectionItem KEY##_f = {(void *)&KEY,true,AUTORELEASE,TAG}; \
+static void KEY(void) \
+
+#define MACH_O_SECTION_FUNCTION_DEFINITION(KEY) MACH_O_SECTION_FUNCTION_DEFINITION_F(KEY,false,@"FUNC")
+
+// using mach-o section to execute function or read pre defined value
+@interface MachOSection : NSObject
+
++ (instancetype)new NS_UNAVAILABLE;
+- (instancetype)init NS_UNAVAILABLE;
++ (instancetype)shareInstance;
+
+- (void)executeFunctionsForKey:(NSString *)key;
+
+- (MachOSectionItem *)sectionItemsForKey:(NSString *)key getCount:(NSUInteger *)count;
+
+@end
+
+NS_ASSUME_NONNULL_END
